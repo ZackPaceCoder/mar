ee1025df2980fd56a5459071b517bf8e55366658
codeKID <acuaytv@gmail.com>
2018-01-03 20:16:15 -0700

    Finish executer.

--- main.sh
+++ main.sh
@@ -3,40 +3,5 @@ do
   echo -n "$>>"
   read inp
   arg=( $(IFS=" " echo "$inp") )
-  if [ "${#arg[@]}" = "1" ]
-  then
-    python programs/${arg[0]}.run ${arg[1]}
-  fi
-  if [ "${#arg[@]}" = "2" ]
-  then
-    python programs/${arg[0]}.run ${arg[1]} ${arg[2]}
-  fi
-  if [ "${#arg[@]}" = "3" ]
-  then
-    python programs/${arg[0]}.run ${arg[1]} ${arg[2]} ${arg[3]}
-  fi
-  if [ "${#arg[@]}" = "4" ]
-  then
-    python programs/${arg[0]}.run ${arg[1]} ${arg[2]} ${arg[3]} ${arg[4]}
-  fi
-  if [ "${#arg[@]}" = "5" ]
-  then
-    python programs/${arg[0]}.run ${arg[1]} ${arg[2]} ${arg[3]} ${arg[4]} ${arg[5]}
-  fi
-  if [ "${#arg[@]}" = "6" ]
-  then
-    python programs/${arg[0]}.run ${arg[1]} ${arg[2]} ${arg[3]} ${arg[4]} ${arg[5]} ${arg[6]}
-  fi
-  if [ "${#arg[@]}" = "7" ]
-  then
-    python programs/${arg[0]}.run ${arg[1]} ${arg[2]} ${arg[3]} ${arg[4]} ${arg[5]} ${arg[6]} ${arg[7]}
-  fi
-  if [ "${#arg[@]}" = "8" ]
-  then
-    python programs/${arg[0]}.run ${arg[1]} ${arg[2]} ${arg[3]} ${arg[4]} ${arg[5]} ${arg[6]} ${arg[7]} ${arg[8]}
-  fi
-  if [ "${#arg[@]}" = "9" ]
-  then
-    python programs/${arg[0]}.run ${arg[1]} ${arg[2]} ${arg[3]} ${arg[4]} ${arg[5]} ${arg[6]} ${arg[7]} ${arg[8]} ${arg[9]}
-  fi
+  python programs/${arg[0]}.run [$arg]
 done
